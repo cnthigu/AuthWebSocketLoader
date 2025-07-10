729536a2409185a0924aac1c62883faabe87dab2
Higor Carniato <higorzen77@gmail.com>
2025-07-10 06:36:36 -0300

    fix: remove cached bin/obj/.vs folders from repo
    fix: remove cached bin/obj/.vs folders from repo

--- ServerWebSocket/ServerWebSocket/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-public class User
-{
-    public int Id { get; set; }
-    public string Username { get; set; } = null!;
-    public string PasswordHash { get; set; } = null!;
-    public string? HWID { get; set; }
-}
